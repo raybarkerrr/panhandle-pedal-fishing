9bd27fab86197d8ca917227b828d3da1a19dfb77
raybarkerrr <raybarkerrr@gmail.com>
2022-11-05 22:10:26 -0500

    Manual Dark Mode functionality 100%
    Functionality for the manual dark mode is finished, I just need to fix the styling and change out the photo for a nicer one and it should be set.

--- javascript.js
+++ javascript.js
@@ -7,6 +7,7 @@ toggleButton.addEventListener("click", () => {
 })
 
 const btn = document.querySelector(".btn-toggle");
+const img = document.querySelector("#button-toggle");
 
 btn.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
@@ -14,5 +15,14 @@ btn.addEventListener("click", function () {
   document.getElementById("fishing2").style.backgroundColor = "#222";
   document.getElementById("ourphotos").style.backgroundColor = "#222";
   document.getElementById("foot").style.backgroundColor = "black"
+
+  
+    if (img.src.match("Photos/moon-regular.svg")) {
+        img.src = "Photos/sun-3-28.png";
+    }
+        else {
+            img.src = "Photos/moon-regular.svg";
+        }
 });
+
 
