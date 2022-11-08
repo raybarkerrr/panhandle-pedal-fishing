69dc4bd0110d1bf438f6b7d08a0d7e95ab480044
raybarkerrr <raybarkerrr@gmail.com>
2022-11-08 12:32:45 -0600

    Reworked JavaScript for working dark mode
    Dark mode is now fully working

--- javascript.js
+++ javascript.js
@@ -3,30 +3,21 @@ const navbarLinks = document.querySelector(".navbar-links")
 
 toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle("active")
-    
+
 })
 
 const btn = document.querySelector(".btn-toggle");
 const img = document.querySelector("#button-toggle");
 
+// Here I am assigning some JS variable to store whether I am in dark mode.
+let isThemeDark = false;
+
 btn.addEventListener("click", function () {
-  document.body.classList.toggle("dark-theme");
-  document.getElementById("aboutus").style.backgroundColor = "#222";
-  document.getElementById("fishing2").style.backgroundColor = "#222";
-  document.getElementById("ourphotos").style.backgroundColor = "#222";
-  document.getElementById("foot").style.backgroundColor = "black"
-
-  
-    if (img.src.match("Photos/moon-regular.svg")) {
-        img.src = "Photos/icons8-sun-30.png";
-    }
-        else {
-            img.src = "Photos/moon-regular.svg";
-        }
-
-        document.body.classList.toggle("light-theme");
-        document.getElementById("aboutus").style.backgroundColor = "#fff";
-        document.getElementById("fishing2").style.backgroundColor = "#fff";
-        document.getElementById("ourphotos").style.backgroundColor = "#fff";
-        document.getElementById("foot").style.backgroundColor = "black"
+  isThemeDark = !isThemeDark;
+  if (isThemeDark) {
+    img.src = "Photos/sun-solid.svg";
+  } else {
+    img.src = "Photos/moon-regular.svg";
+  }
+  document.body.className = isThemeDark ? "dark-theme" : "";
 });
